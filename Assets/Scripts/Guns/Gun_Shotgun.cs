e918f4dc4eac521f6835491a7eddd64daff98400
RafalPankowski.komp <rafal.pankowski.pks@gmail.com>
2022-10-28 19:44:54 +0200

    Implement moveable background

--- Assets/Scripts/Guns/Gun_Shotgun.cs
+++ Assets/Scripts/Guns/Gun_Shotgun.cs
@@ -6,15 +6,18 @@ public class Gun_Shotgun : Gun
 {
     public Ball ballPrefarb;
     public int[] bulletAmount = new int[] { 1, 2, 3 };
+    private float spread = 20f;
 
     protected override void Shoot()
     {
+        var y = bulletAmount[weaponLevel];
+        var x = spread/y;
         base.Shoot();
         for (int i = 0; i < bulletAmount[weaponLevel]; i++)
         {
             Ball ball = Instantiate(ballPrefarb, this.transform.position, transform.rotation);
             ball.weaponLevel = weaponLevel;
-            ball.transform.Rotate(new Vector3(0, 0,  ((-10f * i)/2) + (10f * i)  ));
+            ball.transform.Rotate(new Vector3(0, 0, -x*(2+i-i*y)));
             ball.Project(ball.transform.up);
         }
     }
